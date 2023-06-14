08aaf1d7778c836b3299a4eeed82727456ee9523
Jesús CF <jesus3928cf@gmail.com>
2023-06-13 23:56:49 -0500

    Ventas Detalle de ventas y compras finalizados

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ const routerCompra = require("./routesCompra")
 const routerDetalleCompra = require("./routesDetalleCompra")
 const routerProveedor = require("./routesProveedor")
 
+const routerClientes = require("./routesClientes");
+const routerVentas = require("./routesVentas");
+const routerDetalleVenta = require('./routesDetalleVenta');
+
+
 
 // const routerRoles = require("./routesMovies")
 // const routerConfiguracion = require("./routesMovies")
@@ -13,9 +18,15 @@ function routerApi(app){
     app.use("/usuarios", routerUsuarios);
     app.use("/roles", routerRoles);
     app.use("/configuracion", routerConfiguracion);
-    app.use("/Compra", routerCompra);
-    app.use("/DetalleCompra", routerDetalleCompra);
-    app.use("/Proveedor", routerProveedor);
+    app.use("/compra", routerCompra);
+    app.use("/detalleCompra", routerDetalleCompra);
+    app.use("/proveedor", routerProveedor);
+
+    app.use("/clientes", routerClientes);
+    app.use('/ventas', routerVentas);
+    app.use('/detalleVentas', routerDetalleVenta);
+
+
 }
 
 module.exports = routerApi; 
---
+++ services/VentasService.js
@@ -0,0 +1,164 @@
+const { MongoClient, ObjectId } = require('mongodb');
+require('dotenv').config();
+const uri = process.env.URI;
+const port = process.env.PORT || 4000;
+
+class CompraService {
+    constructor() {}
+
+    //-----------------READ FIND--------------
+
+    async find() {
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            const resultado = await client
+                .db('colch_star')
+                .collection('venta')
+                .find({})
+                .limit(10)
+                .toArray();
+            return resultado;
+        } catch (e) {
+            console.log(e);
+        } finally {
+            await client.close();
+        }
+    }
+
+    //------------------------READ findOne----------------
+
+    async findOne(id) {
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            const resultado = await client
+                .db('colch_star')
+                .collection('venta')
+                .findOne({ _id: new ObjectId(id) });
+            return resultado;
+        } catch (e) {
+            console.log(e);
+        } finally {
+            await client.close();
+        }
+    }
+
+    //----------------------InsertOne------------------------------
+
+    // async insertOne(body){
+    //     const client = new MongoClient(uri);
+    //         try {
+    //             await client.connect();
+    //             const resultado =  await client.db('colch_star').collection('venta').insertOne(body);
+    //             return resultado;
+    //         } catch (e) {
+    //             console.log(e);
+    //         }finally{
+    //             await client.close();
+    //         }
+    // }
+
+    //----------------------InsertMany------------------------------
+
+    async insertMany(body) {
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            const resultado = await client
+                .db('colch_star')
+                .collection('venta')
+                .insertMany(body);
+            return resultado;
+        } catch (e) {
+            console.log(e);
+        } finally {
+            await client.close();
+        }
+    }
+
+    //---------------------updateOne------------
+
+    async updateOne(id, id_venta, precio_total, estado_de_venta) {
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            const resultado = await client
+                .db('colch_star')
+                .collection('Compra')
+                .updateOne(
+                    { _id: new ObjectId(id) },
+                    {
+                        $set: {
+                            precio_total: precio_total,
+                            estado_de_venta: estado_de_venta,
+                            id_venta: id_venta,
+                        },
+                    }
+                );
+            return resultado;
+        } catch (e) {
+            console.log(e);
+        } finally {
+            await client.close();
+        }
+    }
+
+    //--------------------------------UpdateMany------------------------------
+
+    async updateMany(body) {
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            const resultado = await client
+                .db('colch_star')
+                .collection('venta')
+                .updateMany({}, { $set: { body } });
+
+            return resultado;
+        } catch (e) {
+            console.log(e);
+        } finally {
+            await client.close();
+        }
+    }
+
+    // ----------------------------------------------DeleteOne------------------------------
+
+    async deleteOne(id) {
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            const resultado = await client
+                .db('colch_star')
+                .collection('venta')
+                .deleteOne({ _id: new ObjectId(id) });
+            return resultado;
+        } catch (e) {
+            console.log(e);
+        } finally {
+            await client.close();
+        }
+    }
+
+    //----------------------------------------DeleteMany--------------------
+
+    async deleteMany(body) {
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            const resultado = await client
+                .db('colch_star')
+                .collection('venta')
+                .deleteMany(body);
+
+            return resultado;
+        } catch (e) {
+            console.log(e);
+        } finally {
+            await client.close();
+        }
+    }
+}
+
+module.exports = CompraService;
