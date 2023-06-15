ac5484ce579fd99c34aad66eb3815a32e6c0dae9
Brian-3112 <pepito123@gmail.com>
2023-06-14 19:18:36 -0500

    modificacion

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const routerApi = require("./routes");
 const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
-const {engine} = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const path = require('path');
 require("dotenv").config();
 
 const app = express();
 
-const responApi=require('./routes/index')
+const responApi = require('./routes/index')
 
 
 
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 
 //Middlewares
 app.use(bodyparser.json()); // Para poder trabajar con json
-app.use(bodyparser.urlencoded({extended: true})); // Para poder trabajr con formularios codificados en url
+app.use(bodyparser.urlencoded({ extended: true })); // Para poder trabajr con formularios codificados en url
 app.use(express.json()); // Para poder trabajar con json
 app.use(helmet());
 app.use(cors());
@@ -30,7 +30,7 @@ app.use(morgan('tiny'));
 app.use(express.static('public'));
 
 
-app.set('views',path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', engine());
 // app.set('view engine', 'handlebars');
 
@@ -39,21 +39,21 @@ routerApi(app);
 
 // const port = 3000;
 
-app.get('/', (req, res)=>{
-    res.sendFile('views/index.html',{
-        root:__dirname
+app.get('/', (req, res) => {
+    res.sendFile('views/index.html', {
+        root: __dirname
     })
 })
 
-app.get('/api/v1',(req,res)=>{
+app.get('/api/v1', (req, res) => {
     res.status(200).send('API DE COLCH STAR')
 })
 
-app.use('/*',(req,res)=>{
+app.use('/*', (req, res) => {
     res.status(404).send("Paso algo inesperado")
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`El servidor esta escuchando en http://localhost:${port}`);
 })
 
